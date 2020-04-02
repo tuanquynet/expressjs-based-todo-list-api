2fcd291f6e1b7f6e6648e82b2879fd7f74086512
Quy Tran <tuanquynet@gmail.com>
2020-04-02 22:19:17 +0700

    correct MONGO_URI correct npm script in package.json

--- app.js
+++ app.js
@@ -16,7 +16,10 @@ app.use(cors());
 
 // connect mongodb
 //const MONGO_URI = 'mongodb://localhost:27017/todo';
-const MONGO_URI = 'mongodb+srv://devops:share123@cluster0-5kuqc.mongodb.net/todo?retryWrites=true';
+// const MONGO_URI = 'mongodb+srv://devops:share123@cluster0-5kuqc.mongodb.net/todo?retryWrites=true';
+const {
+    MONGO_URI = 'mongodb://localhost:27017/todo'
+} = process.env;
 
 mongoose.connect(MONGO_URI, { useNewUrlParser: true }, () => {
     console.log('Connect to MongoDB @', MONGO_URI, ' successfully.');
